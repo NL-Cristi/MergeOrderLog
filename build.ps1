fcba9330f06fb02260497bc0f92eb99065ac0df9
Cristian Negulescu <laurentiu.negulescu@controlup.com>
2024-10-10 16:44:40 +0100

    win build release folder added

--- build.ps1
+++ build.ps1
@@ -2,12 +2,12 @@
 Write-Host "Building for Linux..."
 $env:GOOS = "linux"
 $env:GOARCH = "amd64"
-go build -o MergeOrderLog main.go
+go build -o  .\release\MergeOrderLog main.go
 
 # Build for Windows
 Write-Host "Building for Windows..."
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
-go build -o MergeOrderLog.exe main.go
+go build -o  .\release\MergeOrderLog.exe main.go
 
 Write-Host "Builds completed."
