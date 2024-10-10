454aa7b57086bc9ac120f339c11d105fd702013d
Cristian Negulescu <cristian@clamsen.com>
2024-10-10 16:39:52 +0100

    build changes

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 Write-Host "Building for Linux..."
 $env:GOOS = "linux"
 $env:GOARCH = "amd64"
-go build -o MergeOrderLog-linux main.go
+go build -o MergeOrderLog main.go
 
 # Build for Windows
 Write-Host "Building for Windows..."
--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # Build for Linux
 echo "Building for Linux..."
-GOOS=linux GOARCH=amd64 go build -o MergeOrderLog-linux main.go
+GOOS=linux GOARCH=amd64 go build -o ./release/MergeOrderLog main.go
 
 # Build for Windows
 echo "Building for Windows..."
-GOOS=windows GOARCH=amd64 go build -o MergeOrderLog.exe main.go
+GOOS=windows GOARCH=amd64 go build -o ./release/MergeOrderLog.exe main.go
 
 echo "Builds completed."
